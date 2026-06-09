d52d3d58274c07c21ffc9756de0d9b5f931046ac
Juliette Print <julietteprint24@gmail.com>
2026-06-09 04:27:44 -0600

    Agregar iconos PNG para PWA

--- vite.config.ts
+++ vite.config.ts
@@ -19,9 +19,14 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "/logo.ico",
-            sizes: "64x64",
-            type: "image/x-icon",
+            src: "/logo-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
